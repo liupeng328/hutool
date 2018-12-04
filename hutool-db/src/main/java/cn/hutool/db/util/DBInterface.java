ff0a9f808ed75d2f4301fa4ece7f401cb78495a1
liupeng <1029538990@qq.com>
2018-12-04 22:17:18 +0800

    JavaBean

--- hutool-db/src/main/java/cn/hutool/db/util/DBInterface.java
+++ hutool-db/src/main/java/cn/hutool/db/util/DBInterface.java
@@ -6,7 +6,6 @@ import cn.hutool.db.Page;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * 关系型数据库操作工具类
@@ -32,9 +31,26 @@ import java.util.function.Consumer;
  */
 interface DBInterface {
 
-    //--------------单表操作常用方法 开始----------------------------------
+    //--------------ActiveRecord 领域模型 开始----------------------------------
 
-    ////------ActiveRecord 领域模型
+    ////------插入操作
+
+    /**
+     * 执行insert sql语句
+     *
+     * @param sql sql语句
+     * @return 成功true，失败false
+     */
+    Boolean insert(String sql);
+
+    /**
+     * 执行insert sql语句
+     *
+     * @param entity 实体对象
+     * @param sql    sql语句
+     * @return 成功true，失败false
+     */
+    Boolean insert(String sql, Entity entity);
 
     /**
      * 插入数据 Entity格式的数据
@@ -69,6 +85,8 @@ interface DBInterface {
      * @return 成功true，失败false
      */
     Collection<Entity> insertBatchPrimaryKey(Collection<Entity> collection);
+
+    ////--------插入或修改操作
 
     /**
      * 插入或修改数据 Entity格式的数据
@@ -106,8 +124,28 @@ interface DBInterface {
      */
     Boolean insertOrUpdateBatchPrimaryKey(Collection<Entity> collection);
 
+    ////--------修改操作
+
+    /**
+     * 执行update sql语句
+     *
+     * @param sql sql语句
+     * @return 成功true，失败false
+     */
+    Boolean update(String sql);
+
+    /**
+     * 执行update sql语句
+     *
+     * @param entity 实体对象
+     * @param sql    sql语句
+     * @return 成功true，失败false
+     */
+    Boolean update(String sql, Entity entity);
+
     /**
      * 通过id选择性修改，只修实体对象不为空的字段  Entity类型数据
+     *
      * @param entity 实体对象
      * @return true成功，false失败
      */
@@ -115,6 +153,7 @@ interface DBInterface {
 
     /**
      * 强制修改全部字段 Entity类型数据
+     *
      * @param entity 实体对象
      * @return true成功，false失败
      */
@@ -122,6 +161,7 @@ interface DBInterface {
 
     /**
      * 批量修改，通过id选择性修改，只修实体对象不为空的字段  Entity类型数据
+     *
      * @param collection 实体对象集合
      * @return true成功，false失败
      */
@@ -129,13 +169,34 @@ interface DBInterface {
 
     /**
      * 批量修改，强制修改全部字段 Entity类型数据
+     *
      * @param collection 实体对象集合
      * @return true成功，false失败
      */
     Boolean updateAllFieldBatch(Collection<Entity> collection);
 
+    ////--------删除操作
+
+    /**
+     * 执行delete sql语句
+     *
+     * @param sql sql语句
+     * @return 成功true，失败false
+     */
+    Boolean delete(String sql);
+
+    /**
+     * 执行delete sql语句
+     *
+     * @param entity 实体对象
+     * @param sql    sql语句
+     * @return 成功true，失败false
+     */
+    Boolean delete(String sql, Entity entity);
+
     /**
      * 通过id删除
+     *
      * @param id 主键
      * @return true成功，false失败
      */
@@ -144,6 +205,7 @@ interface DBInterface {
     /**
      * 通过id逻辑删除
      * 逻辑删除要求表中必须有deleted字段，1为已经删除 0为未删除 deleted默认值为0
+     *
      * @param id 主键
      * @return true成功，false失败
      */
@@ -151,21 +213,26 @@ interface DBInterface {
 
     /**
      * 批量删除，通过id删除
+     *
      * @param ids 主键集合
      * @return true成功，false失败
      */
     Boolean deleteByIdBatch(Collection<Serializable> ids);
 
     /**
-     * 通过id逻辑删除
+     * 批量通过id逻辑删除
      * 逻辑删除要求表中必须有deleted字段，1为已经删除 0为未删除 deleted默认值为0
+     *
      * @param ids 主键集合
      * @return true成功，false失败
      */
     Boolean deleteByIdBatchLogic(Collection<Serializable> ids);
 
+    ////--------查询操作
+
     /**
      * 通过id查询 数据类型为Entity
+     *
      * @param id 主键
      * @return 实体对象
      */
@@ -173,6 +240,7 @@ interface DBInterface {
 
     /**
      * 通过多个id查询 数据类型为Entity
+     *
      * @param ids 主键集合
      * @return 实体对象
      */
@@ -181,6 +249,7 @@ interface DBInterface {
 
     /**
      * 查询一条数据 数据类型为Entity
+     *
      * @param sql sql语句
      * @return 实体对象
      */
@@ -188,14 +257,16 @@ interface DBInterface {
 
     /**
      * 查询一条数据 数据类型为Entity
-     * @param sql sql语句
+     *
+     * @param sql    sql语句
      * @param entity 实体对象
      * @return 实体对象
      */
-    List<Entity> selectOne(String sql,Entity entity);
+    List<Entity> selectOne(String sql, Entity entity);
 
     /**
      * 统计数量
+     *
      * @param sql sql语句
      * @return 实体对象
      */
@@ -203,14 +274,16 @@ interface DBInterface {
 
     /**
      * 统计数量 数据类型为Entity
-     * @param sql sql语句
+     *
+     * @param sql    sql语句
      * @param entity 实体对象
      * @return 实体对象
      */
-    Integer selectCount(String sql,Entity entity);
+    Integer selectCount(String sql, Entity entity);
 
     /**
      * 查询，返回列表 数据类型为Entity
+     *
      * @param sql sql语句
      * @return 实体对象
      */
@@ -218,14 +291,16 @@ interface DBInterface {
 
     /**
      * 查询，返回列表 数据类型为Entity
-     * @param sql sql语句
+     *
+     * @param sql    sql语句
      * @param entity 实体对象
      * @return 实体对象
      */
-    List<Entity> select(String sql,Entity entity);
+    List<Entity> select(String sql, Entity entity);
 
     /**
      * 查询全部数据 数据类型为Entity
+     *
      * @param tableName 表名
      * @return 实体对象
      */
@@ -233,26 +308,222 @@ interface DBInterface {
 
     /**
      * 分页查询，返回列表 数据类型为Entity
+     *
      * @param page 分页对象
-     * @param sql sql语句
+     * @param sql  sql语句
      * @return 实体对象
      */
-    List<Entity> selectPage( Page page,String sql);
+    List<Entity> selectPage(Page page, String sql);
 
     /**
      * 分页查询，返回列表 数据类型为Entity
-     * @param page 分页对象
-     * @param sql sql语句
+     *
+     * @param page   分页对象
+     * @param sql    sql语句
      * @param entity 实体对象
      * @return 实体对象
      */
-    List<Entity> selectPage( Page page,String sql,Entity entity);
+    List<Entity> selectPage(Page page, String sql, Entity entity);
 
-    void transaction(Consumer consumer);
+    /**
+     * 事物操作
+     *
+     * @param transactionFunction 功能函数，这里可以直接传一段代码做参数
+     *                            示例
+     *                            DB.transaction(
+     *                            ()->{
+     *                            //insert
+     *                            //update
+     *                            //你的代码逻辑
+     *                            }
+     *                            );
+     */
+    void transaction(TransactionFunction transactionFunction);
 
 
+    /**
+     * 执行sql
+     *
+     * @param sql sql语句
+     * @return 成功true，失败false
+     */
+    Boolean executeSql(String sql);
 
-    //--------------单表操作常用方法 结束----------------------------------
+    /**
+     * 执行sql
+     *
+     * @param sql    sql语句
+     * @param entity 实体对象
+     * @return Integer 数字：数据库受影响的行数
+     */
+    Integer executeSql(String sql, Entity entity);
+
+
+    //--------------ActiveRecord 领域模型 结束----------------------------------
+
+    //todo orm操作
+    //--------------orm 对象关系型映射 开始----------------------------------
+
+    ////------插入操作
+
+    /**
+     * 插入数据 JavaBean格式的数据
+     *
+     * @param bean JavaBean对象
+     * @return 成功true，失败false
+     */
+    Boolean insert(Object bean);
+
+    /**
+     * 插入数据并返回主键 JavaBean格式的数据
+     * 返回的主键只能是数据库自动生成的，如果需要Java代码生成主键，请操作Entity
+     *
+     * @param bean JavaBean对象
+     * @return 成功true，失败false
+     */
+    Entity insertPrimaryKey(Object bean);
+
+    /**
+     * 批量插入数据 JavaBean格式的数据
+     *
+     * @param collection JavaBean对象集合
+     * @return 成功true，失败false
+     */
+    Boolean insertBatch(Collection<?> collection);
+
+    /**
+     * 批量插入数据并返回主键 JavaBean格式的数据
+     * 返回的主键只能是数据库自动生成的，如果需要Java代码生成主键，请操作Entity
+     *
+     * @param collection JavaBean对象集合
+     * @return 成功true，失败false
+     */
+    Collection<?> insertBatchPrimaryKey(Collection<?> collection);
+
+    ////--------插入或修改操作
+
+    /**
+     * 插入或修改数据 JavaBean格式的数据
+     * 数据不存在时执行插入操作，数据存在时执行修改操作，不为空的字段一律修改
+     *
+     * @param bean JavaBean对象
+     * @return 成功true，失败false
+     */
+    Boolean insertOrUpdate(Object bean);
+
+    /**
+     * 插入或修改数据并返回主键 JavaBean格式的数据
+     * 数据不存在时执行插入操作，数据存在时执行修改操作，不为空的字段一律修改
+     *
+     * @param bean JavaBean对象
+     * @return 成功true，失败false
+     */
+    Boolean insertOrUpdatePrimaryKey(Object bean);
+
+    /**
+     * 批量插入或修改数据 JavaBean格式的数据
+     * 数据不存在时执行插入操作，数据存在时执行修改操作，不为空的字段一律修改
+     *
+     * @param collection JavaBean对象集合
+     * @return 成功true，失败false
+     */
+    Boolean insertOrUpdateBatch(Collection<?> collection);
+
+    /**
+     * 批量插入或修改数据并返回主键 JavaBean格式的数据
+     * 数据不存在时执行插入操作，数据存在时执行修改操作，不为空的字段一律修改
+     *
+     * @param collection JavaBean对象集合
+     * @return 成功true，失败false
+     */
+    Boolean insertOrUpdateBatchPrimaryKey(Collection<?> collection);
+
+    ////--------修改操作
+
+    /**
+     * 通过id选择性修改，只修实体对象不为空的字段  JavaBean类型数据
+     *
+     * @param bean JavaBean对象
+     * @return true成功，false失败
+     */
+    Boolean update(Object bean);
+
+    /**
+     * 强制修改全部字段 JavaBean类型数据
+     *
+     * @param bean JavaBean对象
+     * @return true成功，false失败
+     */
+    Boolean updateAllField(Object bean);
+
+    /**
+     * 批量修改，通过id选择性修改，只修实体对象不为空的字段  JavaBean类型数据
+     *
+     * @param collection JavaBean对象集合
+     * @return true成功，false失败
+     */
+    Boolean updateBatch(Collection<?> collection);
+
+    /**
+     * 批量修改，强制修改全部字段 JavaBean类型数据
+     *
+     * @param collection JavaBean对象集合
+     * @return true成功，false失败
+     */
+    Boolean updateAllFieldBatch(Collection<?> collection);
+
+    /**
+     * 查询一条数据 JavaBean数据类型
+     *
+     * @param sql    sql语句
+     * @param bean JavaBean对象
+     * @return 实体对象
+     */
+    List<Entity> selectOne(String sql, Object bean);
+
+
+    /**
+     * 统计数量 JavaBean数据类型
+     *
+     * @param sql    sql语句
+     * @param bean JavaBean对象
+     * @return 实体对象
+     */
+    Integer selectCount(String sql, Object bean);
+
+    /**
+     * 查询，返回列表 JavaBean数据类型
+     *
+     * @param sql    sql语句
+     * @param bean JavaBean对象
+     * @return 实体对象
+     */
+    List<Entity> select(String sql, Object bean);
+
+
+    /**
+     * 分页查询，返回列表 JavaBean数据类型
+     *
+     * @param page   分页对象
+     * @param sql    sql语句
+     * @param bean JavaBean对象
+     * @return 实体对象
+     */
+    List<Entity> selectPage(Page page, String sql, Object bean);
+
+
+    /**
+     * 执行sql
+     *
+     * @param sql    sql语句
+     * @param bean JavaBean对象
+     * @return Integer 数字：数据库受影响的行数
+     */
+    Integer executeSql(String sql, Object bean);
+
+
+    //--------------ActiveRecord 领域模型 结束----------------------------------
+
 
 
 }
--- hutool-db/src/main/java/cn/hutool/db/util/TransactionFunction.java
+++ hutool-db/src/main/java/cn/hutool/db/util/TransactionFunction.java
@@ -2,10 +2,10 @@ package cn.hutool.db.util;
 
 /**
  * 事务接口，封装一段java代码，用于处理事务操作
- * Java8以上版本特有Lamdba表达式
+ * Java8以上版本特有lamdba表达式
  * 封装事务操作的一段Java代码
  */
 @FunctionalInterface
-public interface Transaction {
-     void test();
+public interface TransactionFunction {
+     void call();
 }
